2f833b05e9d92ed6213a0d608b61c62d1087a21c
Jouni Sunnari <dz4tune@gmail.com>
2021-02-13 23:00:51 +0300

    implemented iterator

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,63 @@
 struct RingBuffer<T> {
-    data_: Box<[T]>
+    data_: Vec<Option<T>>,
+    head_: usize,
+    len_: usize
 }
 
-impl<T> RingBuffer<T> {
+impl<T> std::ops::Index<usize> for RingBuffer<T> {
+    type Output = T;
+
+    fn index(&self, idx: usize) -> &Self::Output {
+        self.data_[self.head_].as_ref().unwrap()
+    }
+
+}
+
+impl<T: Clone> RingBuffer<T> {
 
     fn with_capacity(n : usize) -> RingBuffer<T> {
-        todo!()
+        RingBuffer { data_ : vec![None; n], head_: 0, len_: 0 }
     }
 
-    fn push(val: T) {
-        todo!()
+    fn push(&mut self, val: T) {
+        if self.len_ == self.data_.len() {
+            // let mut new_data = Vec::<T>::with_capacity(self.len_ * 2 + 1);
+            // for elem in self.into_iter() {
+
+            // }
+        }
     }
 
-    fn pop() {
+    fn pop(&mut self) {
         todo!()
     }
 
 }
 
-impl<T> IntoIterator for RingBuffer<T> {
-    type Item=T;
-    type IntoIter=std::vec::IntoIter<T>;
+struct RingBufferIterator<'a, T> {
+    ring_buffer_: &'a RingBuffer<T>,
+    pos_: usize
+}
 
-    fn into_iter(self) -> <Self as std::iter::IntoIterator>::IntoIter {
-        todo!()
+impl<'a, T> Iterator for RingBufferIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos_ < self.ring_buffer_.len_ {
+            self.pos_ += 1;
+            Some(&self.ring_buffer_[self.pos_])
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a RingBuffer<T> {
+    type Item = &'a T;
+    type IntoIter = RingBufferIterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        RingBufferIterator { ring_buffer_: self, pos_: 0 }
     }
 }
 
